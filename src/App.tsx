b67cb84cad90a5e4f82cf9b3f63b2d3028136289
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-10 20:06:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
           className="font-jetbrains text-sm flex sm:top-2 lg:top-2 sm:right-2
             lg:right-2 absolute border-2 border-border rounded-lg px-2 py-1
             justify-between items-center hover:border-border-hover z-10"
-          href="https://github.com/Stupid-Kimi/another_graph_editor"
+          href="https://github.com/zjx-kimi/GraphEditor"
         >
           {settings.darkMode ? (
             <img
