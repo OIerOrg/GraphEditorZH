8e86e9bface89b43e2709c0a8c8b89f68386b846
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-11 19:12:18 +0800

    Update utils.ts

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,50 +1,39 @@
-export function isInteger(s: string) {
-  return parseInt(s).toString() === s;
+
+export interface Graph {
+  nodes: string[];
+  adj: Map<string, string[]>;
+  rev: Map<string, string[]>;
+  edges: string[];
+  edgeLabels: Map<string, string>;
+  nodeLabels: Map<string, string>;
 }
 
-export function sortNodes(nodes: string[]) {
-  const ints = nodes.filter((s: string) => isInteger(s));
-  const notInts = nodes.filter((s: string) => !isInteger(s));
-
-  ints.sort((x: string, y: string) => parseInt(x) - parseInt(y));
-  notInts.sort();
-
-  return [...ints, ...notInts];
+export interface Settings {
+  labelOffset: number;
+  darkMode: boolean;
+  nodeRadius: number;
+  nodeBorderWidthHalf: number;
+  edgeLength: number;
+  showComponents: boolean;
+  showBridges: boolean;
+  treeMode: boolean;
+  lockMode: boolean;
 }
 
-export function generateRandomGraph(
-  numNodes: number,
-  numEdges: number,
-  directed: boolean
-): ParsedGraph {
-  const nodes = Array.from({ length: numNodes }, (_, i) => `${i + 1}`);
-  const edges: string[][] = [];
-  const adj = new Map<string, string[]>();
-
-  nodes.forEach((node) => {
-    adj.set(node, []);
-  });
-
-  const edgeSet = new Set<string>();
-
-  while (edges.length < numEdges) {
-    const u = nodes[Math.floor(Math.random() * numNodes)];
-    const v = nodes[Math.floor(Math.random() * numNodes)];
-
-    if (u !== v && !edgeSet.has(`${u},${v}`) && !edgeSet.has(`${v},${u}`)) {
-      edges.push([u, v]);
-      edgeSet.add(`${u},${v}`);
-      adj.get(u)!.push(v);
-
-      if (!directed) {
-        adj.get(v)!.push(u);
-      }
-    }
-  }
-
-  return {
-    status: "ok",
-    graph: { nodes, edges, adj },
-  };
+export interface ParsedGraph {
+  status: "OK" | "BAD";
+  graph?: Graph;
 }
 
+export type InputFormat = "edges" | "parentChild";
+
+export type ColorMap = Map<string, number>;
+
+export type CutMap = Map<string, boolean>;
+
+export type Layer = [number, number];
+
+export type LayerMap = Map<string, Layer>;
+
+export type BackedgeMap = Map<string, boolean>;
+export type BridgeMap = Map<string, boolean>;
