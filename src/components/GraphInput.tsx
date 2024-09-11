893118280543a1976f24a21c34496e32be0aaa6d
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-11 19:10:46 +0800

    Great

--- src/components/GraphInput.tsx
+++ src/components/GraphInput.tsx
@@ -1,6 +1,10 @@
-import { parseGraphInputEdges, parseGraphInputParentChild } from "./parseGraphInput";
+import { parseGraphInputEdges } from "./parseGraphInput";
+import { parseGraphInputParentChild } from "./parseGraphInput";
 import { useEffect, useState } from "react";
-import { InputFormat, ParsedGraph, Graph } from "../types";  // 确保正确导入
+
+import { InputFormat, ParsedGraph } from "../types";
+import { Graph } from "../types";
+import { sortNodes } from "./utils";
 
 interface Props {
   graphEdges: Graph;
@@ -27,20 +31,31 @@ export function GraphInput({
 
   const processGraphInput = () => {
     let parsedGraph: ParsedGraph;
+
     let roots = "";
 
     if (!directed) {
       roots =
         inputFormat === "edges"
-          ? (document.getElementById("graphInputRootsEdges") as HTMLTextAreaElement).value
-          : (document.getElementById("graphInputRootsParChild") as HTMLTextAreaElement).value;
+          ? (
+              document.getElementById(
+                "graphInputRootsEdges",
+              ) as HTMLTextAreaElement
+            ).value
+          : (
+              document.getElementById(
+                "graphInputRootsParChild",
+              ) as HTMLTextAreaElement
+            ).value;
     }
 
     if (inputFormat === "edges") {
       parsedGraph = parseGraphInputEdges(
         roots,
-        (document.getElementById("graphInputEdges") as HTMLTextAreaElement).value,
-        (document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement).value
+        (document.getElementById("graphInputEdges") as HTMLTextAreaElement)
+          .value,
+        (document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement)
+          .value,
       );
       if (parsedGraph.status === "BAD") {
         setInputStatus(false);
@@ -51,10 +66,14 @@ export function GraphInput({
     } else {
       parsedGraph = parseGraphInputParentChild(
         roots,
-        (document.getElementById("graphInputParent") as HTMLTextAreaElement).value,
-        (document.getElementById("graphInputChild") as HTMLTextAreaElement).value,
-        (document.getElementById("graphInputEdgeLabels") as HTMLTextAreaElement).value,
-        (document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement).value
+        (document.getElementById("graphInputParent") as HTMLTextAreaElement)
+          .value,
+        (document.getElementById("graphInputChild") as HTMLTextAreaElement)
+          .value,
+        (document.getElementById("graphInputEdgeLabels") as HTMLTextAreaElement)
+          .value,
+        (document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement)
+          .value,
       );
       if (parsedGraph.status === "BAD") {
         setInputStatus(false);
@@ -66,12 +85,16 @@ export function GraphInput({
   };
 
   const processNodeLabels = () => {
-    const currentNodes = (document.getElementById("graphInputCurrentNodes") as HTMLTextAreaElement).value
+    const currentNodes = (
+      document.getElementById("graphInputCurrentNodes") as HTMLTextAreaElement
+    ).value
       .trim()
       .split(/\s+/)
       .filter((u) => u.length);
 
-    const nodeLabels = (document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement).value
+    const nodeLabels = (
+      document.getElementById("graphInputNodeLabels") as HTMLTextAreaElement
+    ).value
       .trim()
       .split(/\s+/)
       .filter((u) => u.length);
@@ -93,7 +116,9 @@ export function GraphInput({
     }
   };
 
-  const handleTextAreaKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const handleTextAreaKeyDown = (
+    e: React.KeyboardEvent<HTMLTextAreaElement>,
+  ) => {
     if (e.key === "Escape") {
       e.currentTarget.blur();
     }
@@ -130,7 +155,7 @@ export function GraphInput({
           }
           readOnly
           className="bg-ovr font-semibold font-jetbrains resize-none border-2
-            rounded-md px-2 py-1 border-border focus:outline-none text-lg
+            rounded-md px-2 py-1 border-single focus:outline-none text-lg
             text-current-nodes border-border w-auto no-scrollbar"
         ></textarea>
 
@@ -144,7 +169,7 @@ export function GraphInput({
           onKeyDown={handleTextAreaKeyDown}
           placeholder="提示：'_' 代表空白标签"
           className="bg-ovr font-semibold font-jetbrains resize-none border-2
-            rounded-md px-2 py-1 border-border focus:outline-none text-lg
+            rounded-md px-2 py-1 border-single focus:outline-none text-lg
             border-border focus:border-border-active placeholder-placeholder
             placeholder:italic w-auto no-scrollbar"
         ></textarea>
@@ -219,36 +244,46 @@ export function GraphInput({
           </label>
         </div>
 
-        <br />
-
         <div className="flex font-light text-sm justify-between">
           <span>
             <span>
-              {directed ? (
+              {!directed ? (
                 <span className="text-selected p-0 hover:cursor-pointer">
-                  有向图
+                  无向边
                 </span>
               ) : (
                 <span
                   className="p-0 hover:cursor-pointer"
-                  onClick={() => setDirected(true)}
+                  onClick={() => {
+                    setDirected(false);
+                    let checkbox = document.getElementById(
+                      "directedCheckbox",
+                    ) as HTMLInputElement;
+                    checkbox.checked = false;
+                  }}
                 >
-                  有向图
+                  无向边
                 </span>
               )}
             </span>
             <span> | </span>
             <span>
-              {!directed ? (
+              {directed ? (
                 <span className="text-selected p-0 hover:cursor-pointer">
-                  无向图
+                  有向边
                 </span>
               ) : (
                 <span
                   className="p-0 hover:cursor-pointer"
-                  onClick={() => setDirected(false)}
+                  onClick={() => {
+                    setDirected(true);
+                    let checkbox = document.getElementById(
+                      "directedCheckbox",
+                    ) as HTMLInputElement;
+                    checkbox.checked = true;
+                  }}
                 >
-                  无向图
+                  有向边
                 </span>
               )}
             </span>
@@ -273,102 +308,190 @@ export function GraphInput({
           </label>
         </div>
 
-        {inputFormat === "edges" ? (
-          <>
-            <h4 className="text-base font-semibold">输入根节点</h4>
-            <textarea
-              wrap="off"
-              name="graphInputRootsEdges"
-              id="graphInputRootsEdges"
-              placeholder="根节点 (可选)"
-              rows={1}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
-            <h4 className="text-base font-semibold">输入边</h4>
-            <textarea
-              wrap="off"
-              name="graphInputEdges"
-              id="graphInputEdges"
-              placeholder="请输入边，按空格隔开"
-              rows={4}
-              onChange={processGraphInput}
-              onKeyDown={handleTextAreaKeyDown}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
-          </>
-        ) : (
-          <>
-            <h4 className="text-base font-semibold">输入根节点</h4>
-            <textarea
-              wrap="off"
-              name="graphInputRootsParChild"
-              id="graphInputRootsParChild"
-              placeholder="根节点 (可选)"
-              rows={1}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
+        <br />
 
-            <h4 className="text-base font-semibold">输入父节点</h4>
-            <textarea
-              wrap="off"
-              name="graphInputParent"
-              id="graphInputParent"
-              placeholder="请输入父节点，按空格隔开"
-              rows={4}
-              onChange={processGraphInput}
-              onKeyDown={handleTextAreaKeyDown}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
+        <h4
+          className={
+            !directed && inputFormat === "edges"
+              ? "text-base font-semibold"
+              : "hidden"
+          }
+        >
+          根节点
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputRootsEdges"
+          id="graphInputRootsEdges"
+          rows={1}
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          className={
+            !directed && inputFormat === "edges"
+              ? `bg-ovr font-semibold font-jetbrains resize-none border-2
+                rounded-md px-2 py-1 border-single focus:outline-none text-lg
+                border-border focus:border-border-active w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
 
-            <h4 className="text-base font-semibold">输入子节点</h4>
-            <textarea
-              wrap="off"
-              name="graphInputChild"
-              id="graphInputChild"
-              placeholder="请输入子节点，按空格隔开"
-              rows={4}
-              onChange={processGraphInput}
-              onKeyDown={handleTextAreaKeyDown}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
+        <h4
+          className={
+            !directed && inputFormat === "parentChild"
+              ? "text-base font-semibold"
+              : "hidden"
+          }
+        >
+          根节点
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputRootsParChild"
+          id="graphInputRootsParChild"
+          rows={1}
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          className={
+            !directed && inputFormat === "parentChild"
+              ? `bg-ovr font-semibold font-jetbrains resize-none border-2
+                rounded-md px-2 py-1 border-single focus:outline-none text-lg
+                border-border focus:border-border-active w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
 
-            <h4 className="text-base font-semibold">输入边权</h4>
-            <textarea
-              wrap="off"
-              name="graphInputEdgeLabels"
-              id="graphInputEdgeLabels"
-              placeholder="提示：输入边权，按空格隔开 (可选)"
-              rows={1}
-              onChange={processGraphInput}
-              onKeyDown={handleTextAreaKeyDown}
-              className="bg-ovr font-semibold font-jetbrains resize-none
-                border-2 rounded-md px-2 py-1 border-single focus:outline-none
-                text-lg border-border focus:border-border-active
-                placeholder-placeholder placeholder:italic w-auto
-                no-scrollbar"
-            ></textarea>
-          </>
-        )}
+        <h4
+          className={
+            inputFormat === "edges" ? "text-base font-semibold" : "hidden"
+          }
+        >
+          边
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputEdges"
+          id="graphInputEdges"
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          rows={8}
+          className={
+            inputFormat === "edges"
+              ? `font-semibold font-jetbrains resize-none border-2 rounded-md
+                px-2 py-1 border-single focus:outline-none text-lg border-border
+                focus:border-border-active bg-ovr w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
+
+        <h4
+          className={
+            inputFormat === "parentChild" ? "text-base font-semibold" : "hidden"
+          }
+        >
+        父亲节点数组
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputParent"
+          id="graphInputParent"
+          rows={1}
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          className={
+            inputFormat === "parentChild"
+              ? `bg-ovr font-semibold font-jetbrains resize-none border-2
+                rounded-md px-2 py-1 border-single focus:outline-none text-lg
+                border-border focus:border-border-active w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
+
+        <h4
+          className={
+            inputFormat === "parentChild" ? "text-base font-semibold" : "hidden"
+          }
+        >
+          孩子节点数字
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputChild"
+          id="graphInputChild"
+          rows={1}
+          defaultValue={"1 2 3 4 5 6 7 8 9"}
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          className={
+            inputFormat === "parentChild"
+              ? `bg-ovr font-semibold font-jetbrains resize-none border-2
+                rounded-md px-2 py-1 border-single focus:outline-none text-lg
+                border-border focus:border-border-active w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
+
+        <h4
+          className={
+            inputFormat === "parentChild" ? "text-base font-semibold" : "hidden"
+          }
+        >
+          边标签
+        </h4>
+        <textarea
+          wrap="off"
+          name="graphInputEdgeLabels"
+          id="graphInputEdgeLabels"
+          rows={1}
+          onChange={processGraphInput}
+          onKeyDown={handleTextAreaKeyDown}
+          className={
+            inputFormat === "parentChild"
+              ? `bg-ovr font-semibold font-jetbrains resize-none border-2
+                rounded-md px-2 py-1 border-single focus:outline-none text-lg
+                border-border focus:border-border-active w-auto no-scrollbar`
+              : "hidden"
+          }
+        ></textarea>
+
+        <div className="flex justify-between">
+          <button
+            className="bg-clear-normal hover:bg-clear-hover
+              active:bg-clear-active inline rounded-md px-2 py-1"
+            onClick={() => {
+              if (inputFormat === "edges") {
+                (
+                  document.getElementById(
+                    "graphInputEdges",
+                  ) as HTMLTextAreaElement
+                ).value = "";
+              } else {
+                (
+                  document.getElementById(
+                    "graphInputParent",
+                  ) as HTMLTextAreaElement
+                ).value = "";
+              }
+              processGraphInput();
+            }}
+          >
+            清空
+          </button>
+          {inputStatus ? (
+            <span
+              className="font-jetbrains bg-format-ok rounded-md text-right px-2
+                py-1 inline"
+            >
+              格式：好的
+            </span>
+          ) : (
+            <span
+              className="font-jetbrains bg-format-bad rounded-md text-right px-2
+                py-1 inline"
+            >
+              格式：不好
+            </span>
+          )}
+        </div>
       </div>
     </>
   );
