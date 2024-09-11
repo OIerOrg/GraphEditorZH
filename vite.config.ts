f4f70507c2c2504f7be6dc592db281594769b42a
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-11 18:10:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://zjx-kimi.github.io/GraphEdito/",
+  base: "https://zjx-kimi.github.io/GraphEditor/",
 })
