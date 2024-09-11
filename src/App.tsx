9aa677b7e834303a692bf68d73f8c7284de8ce7a
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-11 18:06:39 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -83,6 +83,28 @@ function App() {
           )}
           <div className="ml-2">Github</div>
         </a>
+        
+        <a
+          className="font-jetbrains text-sm flex sm:top-2 lg:top-2 sm:right-2
+            lg:right-2 absolute border-2 border-border rounded-lg px-2 py-1
+            justify-between items-center hover:border-border-hover z-10"
+          href="https://github.com/anAcc22/another_graph_editor"
+        >
+          {settings.darkMode ? (
+            <img
+              width={18}
+              src="github-mark/github-mark-white.svg"
+              alt="Github Logo"
+            />
+          ) : (
+            <img
+              width={18}
+              src="github-mark/github-mark.svg"
+              alt="Github Logo"
+            />
+          )}
+          <div className="ml-2">Github</div>
+        </a>
 
         <GraphInput
           graphEdges={graphEdges}
