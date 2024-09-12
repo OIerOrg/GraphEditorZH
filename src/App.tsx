c7c83f0d22839c7bfed5f50c28e3bd6e0565b39a
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-09-12 18:46:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ function App() {
           className="font-jetbrains text-sm flex sm:top-2 lg:top-2 absolute border-2 border-border rounded-lg px-2 py-1
             justify-between items-center hover:border-border-hover z-10"
           href="https://zjx-kimi.github.io/GraphEditorEN/"
-          style={{ right: '19.5%' }}  // 稍微偏左
+          style={{ right: '11.5%' }}  // 稍微偏左
         >
           {settings.darkMode ? (
             <img width={18} src="github-mark/github-mark-white.svg" alt="Github Logo" />
